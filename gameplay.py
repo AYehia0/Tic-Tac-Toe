82b7e2066ec29780df98af7ad3802b5460788510
AYehia0 <ahmedyehia611@gmail.com>
2021-01-29 16:44:29 +0200

    Everything works

--- gameplay.py
+++ gameplay.py
@@ -1,9 +1,13 @@
+from moves import all_moves
+from player import RealPlayer, ComputerPlayer
+
 class TicTacToe:
     def __init__(self):
     #    [[' ', ' ', ' '], 
     #     [' ', ' ', ' '], 
     #     [' ', ' ', ' ']]
         self.board = [[' ' for _ in range(3)] for _ in range(3)]
+        self.winner = None
 
     # Printing the board
     def print_board(self):
@@ -30,4 +34,93 @@ class TicTacToe:
                 if place == ' ':
                     empty_spaces.append((j, i))
         return empty_spaces
+    
+    def make_move(self, place, choice):
+        # return True if the move is valid
+        # [[0,1,2],[3,4,5],[6,7,8]]
+        # Checking if the wanted place is empty
+
+        row, col = place
+        position = self.board[row][col]
+        if position == ' ':
+            self.board[row][col] = choice
+            if self.win(place, choice):
+                self.winner = choice
+            return True
+        return False 
+
+    def get_empty_places(self):
+        count = 0
+        for row in self.board:
+            for col in row:
+                if col == ' ':
+                    count += 1
+        if count <= 0:
+            return False,
+        return True, count
+
+    def win(self, place, choice):
+        # Checking rows
+        rows = self.board[place[0]//3]
+        if all(spot == choice for spot in rows):
+            return True
+
+        # Checking colums
+        col_index = all_moves[place[1]][1]
+        cols = [self.board[i][col_index] for i in range(3)]
+        if all(spot == choice for spot in cols):
+            return True
+
+        # Checking the 2 diagonals
+        # First diagonal from up right to down left
+        d1 = [self.board[i][i] for i in range(3)]
+        if all(spot == choice for spot in d1):
+            return True
+
+        d2 = [self.board[i][2-i] for i in range(3)]
+        if all(spot == choice for spot in d2):
+            return True
+
+        return False
+    
+def play_loop(game, player_x, player_o, print_game=True):
+    
+    place = None
+    if print_game:
+        game.map_board()
+
+    # We can going to play with X
+    choice_letter = 'X'
+    while game.get_empty_places()[0]:
+        if choice_letter == 'O':
+            place = player_o.get_move(game)
+        else:
+            place = player_x.get_move(game)
+        
+        if game.make_move(place, choice_letter):
+            if print_game:
+                print(f"{choice_letter}, moved to {place}")
+                game.print_board()
+                print()
+            
+            # Check the winner 
+            if game.winner:
+                if print_game:
+                    print(choice_letter + ' Wins!!!')
+                return choice_letter
+
+            # Switching players
+            choice_letter = 'X' if  choice_letter == 'O' else 'O'
+
+
+        if print_game:
+            print("Tie")
+
+
+if __name__ == '__main__':
+    player_x = RealPlayer('X')
+    player_y = ComputerPlayer('O')
+    game_ttt = TicTacToe()
+    play_loop(game_ttt, player_x, player_y)
+
 
--- player.py
+++ player.py
@@ -13,7 +13,7 @@ X-Player : playes with X, O-Player : chooses O
 
 import random
 import math
-from gameplay import TicTacToe
+from moves import all_moves
 
 class Player:
     def __init__(self, choice_letter):
@@ -28,45 +28,35 @@ class Player:
 class ComputerPlayer(Player):
 
     def __init__(self,choice):
-        super().__init__(self, choice)
+        super().__init__(choice)
     
     # Picking moves is random for now
-    def get_move(self):
+    def get_move(self, game):
 
         # Random choice for the computer
-        move = random.choice(TicTacToe.check_spaces())
+        move = random.choice(game.check_spaces())
         return move
 
 class RealPlayer(Player):
 
     def __init__(self,choice):
-        super().__init__(self, choice)
+        super().__init__(choice)
     
     # Player chooses the move
-    def get_move(self):
+    def get_move(self, game):
         move = None
         is_valid_move = False
 
         while not is_valid_move:
             # Getting input from user
-            spot = input("Enter a move from (0-8)")
+            spot = input("Enter a move from (0-8): ")
             
             try:
                 move = int(spot)
                 # Crazy shit here
-                c = {
-                    0 : (0,0),
-                    1 : (0,1),
-                    2 : (0,2),
-                    3 : (1,0),
-                    4 : (1,1),
-                    5 : (1,2),
-                    6 : (2,0),
-                    7 : (2,1),
-                    8 : (2,2)
-                }
-                move = c[move]
-                if move not in TicTacToe.check_spaces():
+                
+                move = all_moves[move]
+                if move not in game.check_spaces():
                     raise ValueError("Doesn't exist")
                 
                 is_valid_move = True
